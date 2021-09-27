a446c2aa150d2e15e0fd47f0792f0ff7a73c43ba
Chivova <Chivovaz@ua.fm>
2021-09-27 12:56:43 +0300

    fix render problem movieDetails.js components Cast,Reviews

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,28 +6,32 @@ import s from './MovieCard.module.css';
 export default function MovieCard({ movie }) {
   const { backdrop_path, title, overview, genres, vote_average } = movie;
   const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
-   const history = useHistory();
-   const location = useLocation();
+  const history = useHistory();
+  // const location = useLocation();
 
   return (
     <Fragment>
-
-  <button  type="button" onClick = {()=> (history.push('./'))}>
+      <button type="button" onClick={() => history.push('./')}>
         Go back
       </button>
-   <div>
 
-   <img
-        src={backdrop_path ? `${POSTER_URL}/${backdrop_path}` : "https://dummyimage.com/200x300/b3b3b3/fff.jpg&text=No+image"}
-        alt={title}
-      />
-      <p className={s.text}>User score: {vote_average}</p>
-      <h3 className={s.title}>Overview:</h3>
-      <p> {overview}</p>
-      <h3 className={s.title}> Gentres:</h3>
-      <ul>
-        {genres && genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-      </ul></div>
+      <div>
+        <img
+          src={
+            backdrop_path
+              ? `${POSTER_URL}/${backdrop_path}`
+              : 'https://dummyimage.com/200x300/b3b3b3/fff.jpg&text=No+image'
+          }
+          alt={title}
+        />
+        <p className={s.text}>User score: {vote_average}</p>
+        <h3 className={s.title}>Overview:</h3>
+        <p> {overview}</p>
+        <h3 className={s.title}> Gentres:</h3>
+        <ul>
+          {genres && genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+        </ul>
+      </div>
     </Fragment>
   );
 }
--- src/views/MovieDetailsPage/MovieDetailsPage.js
+++ src/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,12 +1,12 @@
 import { useState, useEffect, Fragment, lazy, Suspense } from 'react';
 import { NavLink, Route, useParams, useRouteMatch } from 'react-router-dom';
-// import Loader from 'react-loader-spinner';
+import Loader from 'react-loader-spinner';
 
 import PageHeading from '../../components/PageHeading';
 import MovieCard from '../../components/MovieCard';
 import moviesApi from '../../api/movies-api';
 
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import s from './MovieDetailsPage.module.css';
 
 const Cast = lazy(() => import('../Cast' /* webpackChunkName: "cast-page" */));
@@ -18,38 +18,43 @@ export default function MovieDetailsPage() {
   const { movieId } = useParams();
   const { url, path } = useRouteMatch();
   const [movieDetails, setMovieDetails] = useState([null]);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // setLoading(true);
+    setLoading(true);
 
-    moviesApi.fetchMovieDetailsById(movieId).then(setMovieDetails);
+    moviesApi
+      .fetchMovieDetailsById(movieId)
+      .then(setMovieDetails)
+      .finally(setLoading(false));
   }, [movieId]);
 
   return (
     <Fragment>
-      {/* {loading && (
+      {loading && (
         <Loader type="Watch" color="#00BFFF" height={50} width={50} />
-      )} */}
-      {movieDetails && <MovieCard movie={movieDetails} />}
-      {/* {movieDetails.title && (
+      )}
+      {movieDetails.title && (
         <PageHeading text={`Фильм ${movieDetails.title}`} />
       )}
-      <Suspense fallback={<h1>LOAD</h1>}>
-        <MovieCard movie={movieDetails} />
-        <NavLink className={s.navLink} to={`${url}/cast`}>
-          Cast
-        </NavLink>
+
+      <MovieCard movie={movieDetails} />
+
+      <NavLink className={s.navLink} to={`${url}/cast`}>
+        Cast
+      </NavLink>
+
+      <NavLink className={s.navLink} to={`${url}/reviews`}>
+        Reviews
+      </NavLink>
+      <Suspense fallback={<h1>Loading</h1>}>
         <Route path={`${path}/cast`}>
           <Cast movieId={movieId} />
         </Route>
-        <NavLink className={s.navLink} to={`${url}/reviews`}>
-          Reviews
-        </NavLink>
         <Route path={`${path}/reviews`}>
           <Reviews movieId={movieId} />
         </Route>
-      </Suspense> */}
+      </Suspense>
     </Fragment>
   );
 }
